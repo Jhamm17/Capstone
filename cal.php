5169d8c2296cf3315cfc1c241c341cf422ec488f
zbradtmi <zbradtmi@iu.edu>
2023-01-29 14:48:59 -0500

    Adding Calendar to pages

---
+++ cal.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendar</title>
+</head>
+<link rel="stylesheet" href="css/styles.css">
+<link rel="stylesheet" href="css/calendar.css">
+<body>
+    <div class="topnav">
+        <a href="home.html"><img class="homeImg" src="Images/homebutton.png" alt="Home"></a>
+        <a href="cal.html">Calendar</a>
+        <a href="chat.html">Chat</a> 
+        <a href="community.html">Community</a> 
+        <a href="intramural.html">Intramural Sports</a> 
+        <a href="live.html">IU Live</a>   
+        <a href="polls.html">Polls</a>
+        <a href="profile.html">Profile</a>      
+    </div>
+    <script src="script.js"></script>
+    
+<div class="container">
+<div class="calendar">
+    <div class="month">
+        <i class="fas fa-angle left prev"></i>
+        <div class="date">
+            <h1>February</h1>
+            <p> Friday February 3rd, 2022</p>
+        </div>
+        <i class="fas fa-angle right next"></i>
+    </div>
+    <div class ="weekdays">
+        <div>Sun</div>
+        <div>Mon</div>
+        <div>Tues</div>
+        <div>Wed</div>
+        <div>Thurs</div>
+        <div>Fri</div>
+        <div>Sat</div>
+    </div>
+    <div class="days">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+        <div>10</div>
+        <div>11</div>
+        <div>12</div>
+        <div>13</div>
+        <div>14</div>
+        <div>15</div>
+        <div>16</div>
+        <div>17</div>
+        <div>18</div>
+        <div>19</div>
+        <div>20</div>
+        <div>21</div>
+        <div>22</div>
+        <div>23</div>
+        <div>24</div>
+        <div>25</div>
+        <div>26</div>
+        <div>27</div>
+        <div>28</div>
+    </div>
+
+</div>
+</div>
+
+
+</body>
+</html>
