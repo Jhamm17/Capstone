47eb13bd12a459efbd53b9622a7cb3c48a6ebeeb
zbradtmi <zbradtmi@iu.edu>
2023-01-29 16:09:36 -0500

    Calendar updates

--- cal.php
+++ cal.php
@@ -13,60 +13,9 @@
         <a href="home.html"><img class="homeImg" src="Images/homebutton.png" alt="Home"></a>
         <a href="cal.php">Calendar</a>  
     </div>
-    <script src="script.js"></script>
+    <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23D50000&ctz=America%2FIndiana%2FIndianapolis&showTitle=0&src=N2I4ZmFmMzQ4MmYyNTc0MmI3MjE2ZjA4NTg0MjZiNTRiMmVhZDI5OTRkNWQzY2Y1NTc0ZTZjMjc1OWMwMzU3OEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23D81B60&color=%230B8043" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
     
-<div class="container">
-<div class="calendar">
-    <div class="month">
-        <i class="fas fa-angle left prev"></i>
-        <div class="date">
-            <h1>February</h1>
-            <p> Friday February 3rd, 2022</p>
-        </div>
-        <i class="fas fa-angle right next"></i>
-    </div>
-    <div class ="weekdays">
-        <div>Sun</div>
-        <div>Mon</div>
-        <div>Tues</div>
-        <div>Wed</div>
-        <div>Thurs</div>
-        <div>Fri</div>
-        <div>Sat</div>
-    </div>
-    <div class="days">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
-        <div>8</div>
-        <div>9</div>
-        <div>10</div>
-        <div>11</div>
-        <div>12</div>
-        <div>13</div>
-        <div>14</div>
-        <div>15</div>
-        <div>16</div>
-        <div>17</div>
-        <div>18</div>
-        <div>19</div>
-        <div>20</div>
-        <div>21</div>
-        <div>22</div>
-        <div>23</div>
-        <div>24</div>
-        <div>25</div>
-        <div>26</div>
-        <div>27</div>
-        <div>28</div>
-    </div>
 
-</div>
-</div>
 
 
 </body>
