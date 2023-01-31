94ff598b3182b58f7c1a8cafc6094c7c3222e941
Sam Northquist <samanort@iu.edu>
2023-01-31 15:36:55 -0500

    add form to createteam, style

--- createteam.php
+++ createteam.php
@@ -1,8 +1,23 @@
 <html>
     <head>
         <title></title>
+        <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
         <h1><center>Intramural Create Team Page!</center></h1>
+        <center><form name="teamcreate" method="POST" action="">
+            <label for="teamname">Team Name: </label>
+            <input type="text" name="teamname"></input><br><br>
+            <label for="sport">Sport: </label>
+            <input type="text" name="sport"></input><br><br>
+            <label for="numplayers">Player amount: </label>
+            <input type="number" name="numplayers"></input><br><br>
+            <label for="league">League: </label>
+            <select name="league" id="league"><br>
+                <option value="Competitive">Competitive</option>
+                <option value="Casual">Casual</option>
+                <option value="Rec">Rec</option><br><br>
+            <br><input type="submit" name="submit" value="submit">
+        </form></center>
     </body>
 </html>
--- intramurals.php
+++ intramurals.php
@@ -1,19 +1,19 @@
 <html>
     <head>
         <title>Intramural Page</title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
         <h1><center>Intramural Home Page!</center></h1>
         <div>
             <center>
-            <a href="createteam.html">
+            <a href="createteam.php">
                 <button>CREATE A TEAM</button>
             </a>
-            <a href="teamsearch.html">
+            <a href="teamsearch.php">
                 <button>JOIN A TEAM</button>
             </a>
-            <a href="intramural_players.html">
+            <a href="intramuralplayers.php">
                 <button>PLAYER SEARCH</button>
             </a>
             </center>
