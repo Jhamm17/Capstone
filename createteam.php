4613d3763bd369a633e3c8920b25459c298572ec
Sam Northquist <samanort@iu.edu>
2023-02-01 14:56:58 -0500

    added php to upload to database

--- createteam.php
+++ createteam.php
@@ -19,5 +19,31 @@
                 <option value="Rec">Rec</option><br><br>
             <br><input type="submit" name="submit" value="submit">
         </form></center>
+        <?php 
+            $db = mysqli_connect("db.luddy.indiana.edu","i494f22_samanort","my+sql=i494f22_samanort","i494f22_samanort") or die("Error connecting to MySQL server.");
+            if(isset($_POST['submit'])){
+                $teamname = $_REQUEST["teamname"];
+                $sport = $_REQUEST["sport"];
+                $numplayers = $_REQUEST["numplayers"];
+                $league = $_REQUEST["league"];
+            }
+            $insertquery = "INSERT INTO Teams(team_name, Sport, League, Num_players) VALUES ('$teamname', '$sport', '$league', '$numplayers')";
+            if(isset($_POST['submit'])){
+                if(isset($teamname)){
+                    if(isset($numplayers)){
+                        if(isset($league)){
+                            if(isset($sport)){
+                                if(mysqli_query($db, $insertquery)){
+                                    echo "data stored successfully";
+                                }else{
+                                    echo "data was unable to be stored";
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            mysqli_close($db);
+        ?>
     </body>
 </html>
