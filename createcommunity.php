620943399ca0be599d524113f91d5dfbd9526f96
Sam Northquist <samanort@iu.edu>
2023-02-13 14:40:15 -0500

    added ccmmunity page w php

---
+++ createcommunity.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Community Create Page</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <h1>community create<h1>
+    </body>
+</html>
---
+++ viewcommunities.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Community View Page</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <h1>Community view </h1>
+    </body>
+</html>
