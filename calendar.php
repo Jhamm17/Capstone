498418c95b5c97533d1ececdfa9602605ce6c169
zbradtmi <zbradtmi@iu.edu>
2023-02-13 16:32:23 -0500

    edits

--- calendar.php
+++ calendar.php
@@ -34,6 +34,17 @@
 
   <link rel="stylesheet" href="css/styles.css">
   <body>
+  <div class="topnav">
+        <a href="home.html"><img class="homeImg" src="Images/homebutton.png" alt="Home"></a>
+        <a href="cal.php">Calendar</a>
+        <a href="chat.php">Chat</a> 
+        <a href="community.html">Community</a> 
+        <a href="intramurals.php">Intramural Sports</a> 
+        <a href="live.html">IU Live</a>   
+        <a href="polls.php">Polls</a>
+        <a href="profile2.php">Profile</a>
+        <a href="https://idp.login.iu.edu/idp/profile/cas/login?service=https://cgi.luddy.indiana.edu/~team36/loign.php">Log-In</a> 
+    </div>
 
     <center><div id='calendar'></div></center>
   </body>
