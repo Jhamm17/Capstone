f3c4d7a7c432d0b2e68c408a6ab062f0db085ad7
zbradtmi <zbradtmi@iu.edu>
2023-02-13 14:13:25 -0500

    edits

--- edit-profile.php
+++ edit-profile.php
@@ -1,21 +1,7 @@
 <form method="post" action="update-profile.php">
-  <label for="name">Name:</label>
-  <input type="text" name="name" value="<?php echo $name ?>" />
 
-  <label for="email">Email:</label>
-  <input type="email" name="email" value="<?php echo $email ?>" />
-
-  <label for="graduation_year">Graduation Year:</label>
-  <input type="number" name="graduation_year" value="<?php echo $graduation_year ?>" />
-
-  <label for="bio">About Me:</label>
-  <textarea name="bio"><?php echo $bio ?></textarea>
-
-  <label for="fav_team">Favorite Team:</label>
-  <input type="text" name="fav_team" value="<?php echo $fav_team ?>" />
-
-  <label for="fav_sport">Favorite Sport:</label>
-  <input type="text" name="fav_sport" value="<?php echo $fav_sport ?>" />
+  <label for="GradYear">Graduation Year:</label>
+  <input type="number" name="GradYear" value="<?php echo $GradYear ?>" />
 
   <input type="submit" value="Update Profile" />
 </form>
@@ -36,9 +22,9 @@ if ($conn->connect_error) {
 }
 
 // Update user data
-$sql = "UPDATE users SET Fname = ?, email = ?, grad_year = ?, bio = ?, fav_team = ?, fav_sport = ? WHERE userid = 1001"; // Replace with the user's ID
+$sql = "UPDATE profile SET GradYear = ?  WHERE userid = 1001"; // Replace with the user's ID
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("ssisssi", $_POST["name"], $_POST["email"], $_POST["graduation_year"], $_POST["bio"], $_POST["fav_team"], $_POST["fav_sport"], $user_id);
+$stmt->bind_param("ssisssi", $_POST["GradYear"], $userid);
 $stmt->execute();
 
 // Close the database connection
