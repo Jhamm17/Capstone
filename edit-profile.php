59c345ba69f823e0916fd70d60fd5092785a9848
zbradtmi <zbradtmi@iu.edu>
2023-02-13 15:22:08 -0500

    updating tests

--- edit-profile.php
+++ edit-profile.php
@@ -1,32 +1,16 @@
 <?php
-// Connect to the database
+// Connect to the database and retrieve the user's data
 $servername = "db.luddy.indiana.edu";
 $username = "i494f22_team36";
 $password = "my+sql=i494f22_team36";
 $dbname = "i494f22_team36";
+
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-}
-
-// Update user data
-if (isset($_POST['name']) && isset($_POST['email'])) {
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $sql = "UPDATE user SET Fname='$name', email='$email' WHERE userid=1001"; // Replace with the user's ID
-  $conn->query($sql);
-}
-
-// Update profile data
-if (isset($_POST['favteam']) && isset($_POST['favsport']) && isset($_POST['grad']) && isset($_POST['bio'])) {
-  $favteam = $_POST['favteam'];
-  $favsport = $_POST['favsport'];
-  $grad = $_POST['grad'];
-  $bio = $_POST['bio'];
-  $sql = "UPDATE profile SET FavTeam='$favteam', FavSport='$favsport', GradYear='$grad', bio='$bio' WHERE userid=1001"; // Replace with the user's ID
-  $conn->query($sql);
 }
 
 // Retrieve user data
@@ -42,8 +26,6 @@ if ($result->num_rows > 0) {
 } else {
   echo "0 results";
 }
-
-// Retrieve profile data
 $sql = "SELECT * FROM profile WHERE userid = 1001"; // Replace with the user's ID
 $result = $conn->query($sql);
 
@@ -58,3 +40,38 @@ if ($result->num_rows > 0) {
 } else {
   echo "0 results";
 }
+
+// Close the database connection
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head> 
+    </head>
+  <link rel="stylesheet" href="css/styles.css">
+  
+  <body>
+  <div class="topnav">
+        <a href="home.html"><img class="homeImg" src="Images/homebutton.png" alt="Home"></a>
+        <a href="cal.php">Calendar</a>
+        <a href="chat.php">Chat</a> 
+        <a href="community.html">Community</a> 
+        <a href="intramurals.php">Intramural Sports</a> 
+        <a href="live.html">IU Live</a>   
+        <a href="polls.php">Polls</a>
+        <a href="profile.php">Profile</a>
+        <a href="https://idp.login.iu.edu/idp/profile/cas/login?service=https://cgi.luddy.indiana.edu/~team36/loign.php">Log-In</a> 
+    </div>
+    <div class="profile">
+    <h1><?php echo $name ?>'s Profile</h1>
+    <h2>Contact Me</h2>
+    <p>Email: <?php echo $email ?></p>
+    <p>Graduation Year: <?php echo $grad ?></p>
+    <p>About Me:<?php echo $bio ?></p>
+    <p>Favorite Team: <?php echo $favteam ?></p>
+    <p>Favorite Sport: <?php echo $favsport ?></p>
+    </div>
+    <a href="edit-profile.php">Edit Profile</a>
+  </body>
+</html>
