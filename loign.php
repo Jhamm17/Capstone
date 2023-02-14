b11f2edd4eec5b827c63325d3a46498480320ba1
hstarnes <hstarnes@iu.edu>
2023-02-14 12:07:42 -0500

    got rid of header line

--- loign.php
+++ loign.php
@@ -10,7 +10,6 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="css/styles.css">
-    <script src="js/site.js"></script>
 </head>
 <body>
 <div class="topnav"> 
