408c5f41f0d0c5e8c31482a003294c987aa819b0
zbradtmi <zbradtmi@iu.edu>
2023-02-20 16:24:47 -0500

    testlogin

--- inserttest.php
+++ inserttest.php
@@ -6,8 +6,13 @@ $password = "my+sql=i494f22_team36";
 $dbname = "i494f22_team36";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
+// Check if the connection was successful
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
 // Check if the form was submitted
-if (isset($_POST['login'])) {
+if (isset($_POST['login']) && isset($_POST['Fname']) && isset($_POST['Lname']) && isset($_POST['email'])) {
     // Retrieve the form data
     $fname = $_POST['Fname'];
     $lname = $_POST['Lname'];
