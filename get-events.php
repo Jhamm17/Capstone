237780bda79a3c145018de8b102a05f8b7be0212
zbradtmi <zbradtmi@iu.edu>
2023-02-20 13:18:49 -0500

    updates

--- get-events.php
+++ get-events.php
@@ -1,23 +1,19 @@
 <?php
-// Connect to the database and retrieve the events
+
 $servername = "db.luddy.indiana.edu";
 $username = "i494f22_team36";
 $password = "my+sql=i494f22_team36";
 $dbname = "i494f22_team36";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// Retrieve events from the database
 $sql = "SELECT title, start, end, description FROM calendar";
 $result = $conn->query($sql);
 
-// Format the events as JSON
 $events = array();
 if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
@@ -31,10 +27,9 @@ if ($result->num_rows > 0) {
 }
 $events = json_encode($events);
 
-// Send the events data as a response
+
 header('Content-Type: application/json');
 echo $events;
 
-// Close the database connection
 $conn->close();
 ?>
