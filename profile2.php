d86804cc86851b94fff9cdab4e29f4d51ba87e10
zbradtmi <zbradtmi@iu.edu>
2023-02-20 14:46:01 -0500

    testing

--- profile2.php
+++ profile2.php
@@ -10,7 +10,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM user WHERE userid = "$userid""; 
+$sql = "SELECT * FROM user WHERE userid = $userid"; 
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
