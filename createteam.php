c15a6ce7028bb64ea1b6a6b226343e4486589603
Sam Northquist <samanort@iu.edu>
2023-02-22 16:43:07 -0500

    edit createteam page

--- createteam.php
+++ createteam.php
@@ -6,13 +6,13 @@
     <body>
     <div class="topnav">
         <a href="home.html"><img class="homeImg" src="Images/homebutton.png" alt="Home"></a>
-        <a href="cal.php">Calendar</a>
+        <a href="calendar.php">Calendar</a>
         <a href="chat.html">Chat</a> 
         <a href="community.html">Community</a> 
         <a href="intramurals.php">Intramural Sports</a> 
         <a href="live.html">IU Live</a>   
         <a href="polls.php">Polls</a>
-        <a href="profile.php">Profile</a>
+        <a href="profile2.php">Profile</a>
         <a href="https://idp.login.iu.edu/idp/profile/cas/login?service=https://cgi.luddy.indiana.edu/~team36/loign.php">Log-In</a> 
     </div>
         <h1><center>Intramural Create Team Page!</center></h1>
