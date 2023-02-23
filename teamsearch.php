e36141c7655fa30905c30e5c47521fee48a9951b
Sam Northquist <samanort@iu.edu>
2023-02-23 15:06:39 -0500

    add stuff to viewteam

--- teamsearch.php
+++ teamsearch.php
@@ -6,14 +6,16 @@
     <body>
     <div class="topnav">
         <a href="home.html"><img class="homeImg" src="Images/homebutton.png" alt="Home"></a>
-        <a href="cal.php">Calendar</a>
+        <a href="calendar.php">Calendar</a>
         <a href="chat.html">Chat</a> 
         <a href="community.html">Community</a> 
         <a href="intramurals.php">Intramural Sports</a> 
         <a href="live.html">IU Live</a>   
         <a href="polls.php">Polls</a>
         <a href="profile.php">Profile</a>
-        <a href='https://idp.login.iu.edu/idp/profile/cas/login?service=https://cgi.luddy.indiana.edu/~team36/login.php'>Log-In</a> 
+        <a href='https://idp.login.iu.edu/idp/profile/cas/login?service=https://cgi.luddy.indiana.edu/~team36/login.php'>Log-In</a>
+        <a href="https://idp.login.iu.edu/idp/profile/cas/logout">Log-Out </a>
+ 
     </div>
         <h1><center>Intramural Team Search Page!</center></h1>
         <center><table class="tabledesign">
@@ -26,10 +28,9 @@
                 mysqli_query($db, $query1) or die('Error querying database.');
                 $result = mysqli_query($db, $query1);
                 $row = mysqli_fetch_array($result);
-                echo '<tr><th>Item id</th><th>Team Name</th><th>Sport</th><th>League</th><th>Number of Players</th></tr>';
+                echo '<tr><th>Team Name</th><th>Sport</th><th>League</th><th>Number of Players</th></tr>';
                 while($row = mysqli_fetch_array($result)){
                     echo "<tr>";
-                    echo "<td>" . $row['Team_id'] . "</td>";
                     echo "<td>" . $row['team_name'] . "</td>";
                     echo "<td>" . $row['Sport'] . "</td>";
                     echo "<td>" . $row['League'] . "</td>";
