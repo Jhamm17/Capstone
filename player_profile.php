206b514f95a93d04bfd8ecb8d47f2a4d3b98984b
zbradtmi <zbradtmi@iu.edu>
2023-02-27 12:47:02 -0500

    adding nav

--- player_profile.php
+++ player_profile.php
@@ -41,6 +41,19 @@ $query = "SELECT Intramurals.user_email, Intramurals.Preferred_sport, Intramural
     ?>
     <link rel="stylesheet" href="css/styles.css">
 <body>
+<div class="topnav"> 
+        <a href="home.html"><img class="homeImg" src="Images/homebutton.png" alt="Home"></a>
+        <a href="calendar.php">Calendar</a>
+        <a href="chat.php">Chat</a> 
+        <a href="community.php">Community</a> 
+        <a href="intramurals.php">Intramural Sports</a> 
+        <a href="live.php">IU Live</a>   
+        <a href="polls.php">Polls</a>
+        <a href="profile2.php">Profile</a>
+        <a href="https://idp.login.iu.edu/idp/profile/cas/login?service=https://cgi.luddy.indiana.edu/~team36/loign.php">Log-In</a> 
+        <a href="https://idp.login.iu.edu/idp/profile/cas/logout">Log-Out </a>
+
+</div>
     <div class="profile">
       <h1><?php echo $row['full_name'] ?>'s Profile</h1>
       <h2>Contact Me</h2>
