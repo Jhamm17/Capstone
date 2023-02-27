f79b1c145c3c39a3a71c551f13cf0a629552e143
hstarnes <hstarnes@iu.edu>
2023-02-26 22:09:27 -0500

    trying to authenticate iu login

--- loign.php
+++ loign.php
@@ -32,41 +32,106 @@
     <!-- parsing examples and help with part three of itp found here: https://code.tutsplus.com/tutorials/how-to-parse-json-in-php--cms-36994 -->
 
 <?php
-if (isset($_GET["ticket"])){
-    $tic = $_GET["ticket"];
-    $request = "https://idp.login.iu.edu/idp/profile/cas/serviceValidate?ticket=" . $tic . "&service=https://cgi.luddy.indiana.edu/~team36/loign.php";
-    $file = file_get_contents($request);
-   // echo $file;
-    //var_dump($file);
-    $dom = new DomDocument();
-    $dom->loadXML($file);
-    $xpath = new DomXPath($dom);
-    $node = $xpath->query("//cas:user");
-    // office hours thursday with makejari
-    if ($node->length){
-        $username=$node[0]->textContent;
-        
-        $_SESSION['username'] = $username;
-        //echo $username;
-        $emailend ='@iu.edu';
-        //$user = substr($file,0,-50);
-        //echo strrev($user);
-        $IUemail =$username.$emailend;
-        //echo $IUemail;
-       // echo $IUemail;
-       // echo $IUemail;
-       $compare = "SELECT * FROM user WHERE email=" . "'" . $IUemail . "'";
-       $query = mysqli_query($conn,$compare);
-        if (mysqli_num_rows($query == 0)){
-            echo "fill out login first";
+session_start();
 
-        }else{
-            echo "logged in";
-        }
-
+function cas_authenticate(){
+    $sid = SID; //SESSION ID
+  
+    //Because SESSIONS are used, I default set it to false to make them login. 
+    //Watch out with this. I used it because I just wanted to authenticate that they are a student
+    //But if you plan to use CAS for the login method, this is a bad idea most likley.
+    $_SESSION['CAS'] = false;
+  
+    $authenticated = $_SESSION['CAS'];
+    //Make sure that your code redirects back to here or else you will get an error.
+    $casurl = "cgi.luddy.indiana.edu/~team36/loign.php";
+  
+    //send user to CAS login if not authenticated
+    if (!$authenticated) {
+      $_SESSION['LAST_SESSION'] = time(); // update last activity time stamp
+      $_SESSION['CAS'] = true;
+      echo '<META HTTP-EQUIV="Refresh" Content="0; URL=https://cas.iu.edu/cas/login?cassvc=IU&casurl='.$casurl.'">';
+  
+      exit;
     }
   
-}
+    if ($authenticated) {
+      if (isset($_GET["casticket"])) {
+        //set up validation URL to ask CAS if ticket is good
+        $_url = 'https://cas.iu.edu/cas/validate';
+        $cassvc = 'IU';
+  
+        $params = "cassvc=$cassvc&casticket=$_GET[casticket]&casurl=$casurl";
+        $urlNew = "$_url?$params";
+  
+        //CAS sending response on 2 lines. First line contains "yes" or "no". If "yes", second line contains username (otherwise, it is empty).
+        $ch = curl_init();
+        $timeout = 5; // set to zero for no timeout
+        curl_setopt ($ch, CURLOPT_URL, $urlNew);
+        curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        ob_start();
+        curl_exec($ch);
+        curl_close($ch);
+        $cas_answer = ob_get_contents();
+        ob_end_clean();
+  
+        //split CAS answer into access and user
+        list($access,$user) = split("\n",$cas_answer,2);
+        $access = trim($access);
+        $user = trim($user);
+  
+        //set user and session variable if CAS says YES
+        if ($access == "yes") {
+          $_SESSION['user'] = $user;
+        }
+  
+      } else if (!isset($_SESSION['user'])) { //END GET CAS TICKET
+        echo '<META HTTP-EQUIV="Refresh" Content="0; URL=https://cas.iu.edu/cas/login?cassvc=IU&casurl='.$casurl.'">';
+      }
+    }
+  }//END CAS FUNCTION
+  
+  cas_authenticate();
+  
+  //gets the username from the SESSION and assigns it to username.
+  $username = $_SESSION['user'];
+  $email_address = $_SESSION['email_address'];
+
+// if (isset($_GET["ticket"])){
+//     $tic = $_GET["ticket"];
+//     $request = "https://idp.login.iu.edu/idp/profile/cas/serviceValidate?ticket=" . $tic . "&service=https://cgi.luddy.indiana.edu/~team36/loign.php";
+//     $file = file_get_contents($request);
+//    // echo $file;
+//     //var_dump($file);
+//     $dom = new DomDocument();
+//     $dom->loadXML($file);
+//     $xpath = new DomXPath($dom);
+//     $node = $xpath->query("//cas:user");
+//     // office hours thursday with makejari
+//     if ($node->length){
+//         $username=$node[0]->textContent;
+        
+//         $_SESSION['username'] = $username;
+//         //echo $username;
+//         $emailend ='@iu.edu';
+//         //$user = substr($file,0,-50);
+//         //echo strrev($user);
+//         $IUemail =$username.$emailend;
+//         //echo $IUemail;
+//        // echo $IUemail;
+//        // echo $IUemail;
+//        $compare = "SELECT * FROM user WHERE email=" . "'" . $IUemail . "'";
+//        $query = mysqli_query($conn,$compare);
+//         if (mysqli_num_rows($query == 0)){
+//             echo "fill out login first";
+
+//         }else{
+//             echo "logged in";
+//         }
+
+//     }
+  
+// }
 //if ($IUemail)
 //https://idp.login.iu.edu/idp/profile/cas/logout
 
