07bd870dd89bd9918ea61b023e679882dd458fd6
Sam Northquist <samanort@iu.edu>
2023-02-27 15:43:14 -0500

    added php to view team rosters

--- viewteam.php
+++ viewteam.php
@@ -16,5 +16,24 @@
                 <a href="https://idp.login.iu.edu/idp/profile/cas/login?service=https://cgi.luddy.indiana.edu/~team36/loign.php">Log-In</a> 
                 <a href="https://idp.login.iu.edu/idp/profile/cas/logout">Log-Out </a>
             </div>
+            <center>
+            <br>
+            <br>
+            <br>
+            <?php
+            $id = $_GET["id"];
+            $db = mysqli_connect("db.luddy.indiana.edu","i494f22_samanort","my+sql=i494f22_samanort","i494f22_samanort") or die("Error connecting to MySQL server.");
+            if (mysqli_connect_errno()){
+                echo 'failed to connect to SQL';
+            }
+            $query = "SELECT * FROM user JOIN team_rosters ON user.userid=team_rosters.Player_id WHERE Team_id='$id'";
+            mysqli_query($db, $query) or die('Error querying database.');
+            $result = mysqli_query($db, $query);
+            echo "<h1>Team Roster</h1>";
+            while($row=mysqli_fetch_array($result)){
+                echo 'Teamid' . $row["Team_id"] . '<br>';
+                echo 'Player email: ' . $row["email"] . '<br>';
+            }
+        ?></center>
     </body>
 </html>
