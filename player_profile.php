1438c75afb66fc27ab0b7b88620e87649a4e4c00
zbradtmi <zbradtmi@iu.edu>
2023-02-27 12:01:13 -0500

    testing

--- player_profile.php
+++ player_profile.php
@@ -6,20 +6,16 @@ $email = $_GET['email'];
 $db = mysqli_connect("db.luddy.indiana.edu","i494f22_samanort","my+sql=i494f22_samanort","i494f22_samanort") or die("Error connecting to MySQL server.");
 
 // Query the database for the player's information based on the email parameter
-$query = "SELECT * FROM Intramurals WHERE user_email = '$email'";
-$query1 = "SELECT * FROM user WHERE user_email = '$email'";
+$query = "SELECT Intramurals.Fname, Intramurals.Preferred_sport, Intramurals.On_team, user.name 
+          FROM Intramurals 
+          JOIN user ON Intramurals.user_email = user.email
+          WHERE Intramurals.user_email = '$email'";
 $result = mysqli_query($db, $query) or die('Error querying database.');
 $row = mysqli_fetch_array($result);
-// Run the second query to get the user's name
-$result1 = mysqli_query($db, $query1) or die('Error querying database.');
-$row1 = mysqli_fetch_array($result1);
-
-// Output the user's name
-echo "<p><b>Name:</b> " . $row1['name'] . "</p>";
 
 // Generate the HTML for the player's profile page
 echo "<h1>Player Profile</h1>";
-// echo "<p><b>Name:</b> " . $row['Fname'] . "</p>";
+echo "<p><b>Name:</b> " . $row['Fname'] . "</p>";
 echo "<p><b>Email:</b> " . $row['user_email'] . "</p>";
 echo "<p><b>Preferred Sport:</b> " . $row['Preferred_sport'] . "</p>";
 echo "<p><b>On Team:</b> " . $row['On_team'] . "</p>";
