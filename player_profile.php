56807ccfcd51cbe546a8ac29b9541260d9861790
zbradtmi <zbradtmi@iu.edu>
2023-02-27 16:49:40 -0500

    css

--- player_profile.php
+++ player_profile.php
@@ -77,7 +77,7 @@ $query = "SELECT Intramurals.user_email, Intramurals.Preferred_sport, Intramural
         <p class="descriptors">Favorite Sport: </p>
         <p class="elements"><?php echo $favsport ?></p>
       </div>
-      <a href="intramuralplayers.php">Return to Player Search</a>
+      <button><a href="intramuralplayers.php">Return to Player Search</a></button>
     </div> 
     </div> 
 </body>
