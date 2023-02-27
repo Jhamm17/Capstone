8abd046e9f42080abecc475a9ab597115737fb60
hstarnes <hstarnes@iu.edu>
2023-02-27 16:40:10 -0500

    testing trim

--- profile2.php
+++ profile2.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM user WHERE email = " . $_SESSION['email']; 
+$sql = "SELECT * FROM user WHERE email = " . trim($_SESSION['email']); 
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
