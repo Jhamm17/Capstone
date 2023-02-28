e34769652128a8f50e5f4d6467b2737b8fbaf8b7
zbradtmi <zbradtmi@iu.edu>
2023-02-27 21:35:02 -0500

    updates

--- loign.php
+++ loign.php
@@ -151,7 +151,7 @@ if(!isset($_SESSION['CAS'])){
          }else{
             //  $userid = "SELECT userid FROM user WHERE email=" . "'" . $email . "'";
             //  $qu = mysqli_query($conn,$userid);
-            header("Location: home.html");
+            header("Location: home.php");
 
             //  $_SESSION['userid'] = $qu;
             //  echo $_SESSION['userid'];
