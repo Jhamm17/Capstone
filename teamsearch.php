a25b14fc8945a92845f115ec900dd476841e4aa7
zbradtmi <zbradtmi@iu.edu>
2023-02-27 19:44:29 -0500

    navbar

--- teamsearch.php
+++ teamsearch.php
@@ -5,7 +5,7 @@
     </head>
     <body>
     <div class="topnav">
-        <a href="home.html"><img class="homeImg" src="Images/homebutton.png" alt="Home"></a>
+        <a href="homepage.html"><img class="homeImg" src="Images/homebutton.png" alt="Home"></a>
         <a href="calendar.php">Calendar</a>
         <a href="chat.html">Chat</a> 
         <a href="community.html">Community</a> 
