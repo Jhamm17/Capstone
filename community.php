d82075ecb5cfe45b6235f52f878436bac78ec10e
Sam Northquist <samanort@iu.edu>
2023-03-08 17:56:12 -0500

    test

--- community.php
+++ community.php
@@ -46,7 +46,7 @@
                         <td><?php echo $row['comm_name'];?></td>
                         <td><?php echo $row['comm_subject']; ?></td>
                         <td><?php echo $row['comm_bio']; ?></td>
-                        <td><a href="viewcommunity.php?id="<?php echo $commid; ?>>Join</a></td>
+                        <td><a href="viewcommunity.php?id=<?php echo $commid; ?>">Join</a></td>
                         <!-- // echo "<td><a href=\"NBAcommunity.php\">View</a></td>"; -->
                         </tr><?php
                     }
