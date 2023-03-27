3ce48ab6c719058491b72174d0febd53449fa1dc
zbradtmi <zbradtmi@iu.edu>
2023-03-27 16:30:55 -0400

    debug

--- About.php
+++ About.php
@@ -18,7 +18,7 @@
 <body>
 <div>
 <h1>About Us</h1>
-<h2> Our Story <h2>
+<h2> Our Story </h2>
 <p> We decided to create a website due to people coming to Indiana University from all across the world and making friends can be difficult.</p> 
 <p>We hope to help Indiana students connect with one another through talking, watching, and playing sports together.</p>
 <h2> Meet the Team <h2>
